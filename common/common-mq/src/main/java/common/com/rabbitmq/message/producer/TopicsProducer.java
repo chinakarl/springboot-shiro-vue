e3353d97dfa81f0e09c6cba90a7d94a5a3bb7232
zhaihaixiang <netzhaihaixiang@sian.com>
2018-12-12 15:36:56 +0800

    rabbitmq 五中模式基本调用方法

---
+++ common/common-mq/src/main/java/common/com/rabbitmq/message/producer/TopicsProducer.java
@@ -0,0 +1,33 @@
+package common.com.rabbitmq.message.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import common.com.rabbitmq.message.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ *
+ * 通配符
+ *
+ * @Author: zhaihx
+ * @Date: Created in 14:56 2018/12/12
+ **/
+public class TopicsProducer {
+
+        private static final String EXCHANGE_NAME = "test_exchange_topic";
+
+        public static void main(String[] args) throws IOException, TimeoutException {
+            Connection connection = ConnectionUtil.getConnection();
+            Channel channel = connection.createChannel();
+            //声明交换机
+            channel.exchangeDeclare(EXCHANGE_NAME,"topic");
+            String message = "匹配insert";
+            channel.basicPublish(EXCHANGE_NAME,"order.update",false,false,null,message.getBytes());
+
+            channel.close();
+            connection.close();
+        }
+
+}
---
+++ common/common-mq/src/main/java/common/com/rabbitmq/message/producer/WorkProducer.java
@@ -0,0 +1,35 @@
+package common.com.rabbitmq.message.producer;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import common.com.rabbitmq.message.util.ConnectionUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author: zhaihx
+ * @Description:
+ * @Date: Created in 9:58 2018/12/12
+ **/
+public class WorkProducer {
+
+    private static final String QUEUE_NAME="test_queue_work";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        //获取连接
+        Connection connection = ConnectionUtil.getConnection();
+        System.out.println(connection);
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        String message = "Hello World!";
+        for (int i = 0; i < 20; i++) {
+            message = message + i;
+            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+        }
+        System.out.println("生产者 send ："+message);
+        channel.close();
+        connection.close();
+    }
+
+}
